113d80484f63c4911ab60913d9afcdd1d989794e
Bruno Cabral <cabral.brc@gmail.com>
2021-11-08 23:18:49 -0800

    map function

---
+++ 39_map_function.py
@@ -0,0 +1,55 @@
+# Loop without a loop
+
+# Basic usage
+people = ['Matt', 'Bryan', 'Tammy', 'Markus']
+
+# old way
+counts = []
+for x in people:
+    counts.append(len(x))
+print(f'Old way: {counts=}')
+
+# modern way
+print(f'Mapped: {list(map(len, people))}')
+
+
+
+# More complex - combine elements
+# Notice different lens, multiple args
+
+first_names = ['Apple', 'Chocolate', 'Fudge', 'Pizza']
+last_names = ['Pie', 'Cake', 'Brownies']
+
+def merg(a, b):
+    return a + ' ' + b
+
+x = map(merg, first_names, last_names)
+print(x)
+print(list(x))
+
+
+
+# Multiple functions = combine functions
+# call multiple functions in one map call
+
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b
+def divide(a, b):
+    return a / b
+
+from typing import Callable
+
+def do_all(func: Callable, num: list):
+    return func(num[0], num[1])
+
+f = (add, subtract, multiply, divide)
+v = [[5,3]]
+n = list(v) * len(f) # [[5, 3], [5, 3], [5, 3], [5, 3]]
+print(f'{f=} , {n=}')
+
+m = map(do_all, f, n)
+print(list(m))
