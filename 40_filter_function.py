08d2085821245a101a8e27d55511832ea113a117
Bruno Cabral <cabral.brc@gmail.com>
2021-11-08 23:30:29 -0800

    filter function

---
+++ 40_filter_function.py
@@ -0,0 +1,58 @@
+# Filter function
+# filter(func, iterable)
+
+# Subrange
+
+import random
+
+v = []
+for x in range(10):
+    v.append(random.randrange(100))
+print(f'{v=}')
+
+def lower(value):
+    if value < 50:
+        return True
+    return False
+
+f = filter(lower, v)
+print(f'Less than 50: {list(f)=}')
+
+
+
+# Filter types
+
+class Animal:
+    def __init__(self, name):
+        self.name = name
+
+class Cat(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+class Dog(Animal):
+    def __init__(self, name):
+        super().__init__(name)
+
+animals = []
+for x in range(10):
+    name = f'Animal {x}'
+    if (x%2) == 0:
+        animals.append(Cat(name))
+    else:
+        animals.append(Dog(name))
+
+print(f'{animals=}')
+
+for a in animals:
+    print(f'Animal: {a.name}')
+
+def cats(animal: Animal):
+    return isinstance(animal, Cat)
+def dogs(animal: Animal):
+    return isinstance(animal, Dog)
+
+for c in list(filter(cats, animals)):
+    print(f'Cat: {c.name}')
+for d in list(filter(dogs, animals)):
+    print(f'Dog: {d.name}')
